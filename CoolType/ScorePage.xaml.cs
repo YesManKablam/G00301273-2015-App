e27ad52782f484b3ecdba6b2e9e1caa15eefc7c2
Conor Kenny <Conor Kenny>
2015-12-22 22:13:25 +0000

    First Commit

---
+++ CoolType/ScorePage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.OneDrive;
+using Microsoft.OneDrive.Sdk;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Popups;
+using Windows.Storage;
+using System.IO;
+using Windows.Storage.Pickers;
+using System.Threading.Tasks;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CoolType
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ScorePage : Page
+    {
+        IOneDriveClient myOneDrive;
+        StorageFolder storageFolder;
+        StorageFile sampleFile;
+        String test = "test";
+
+        private readonly string[] scopes =
+                            new string[]
+                            {
+                                "onedrive.appfolder",
+                                           "onedrive.readwrite",
+                                           "wl.offline_access",
+                                           "wl.signin",
+                                           "wl.skydrive",
+                                           "wl.skydrive_update",
+                                           "onedrive.appfolder"
+                            };
+
+        public ScorePage()
+        {
+            this.InitializeComponent();
+
+        }
+        public Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        private async void getScoreBtn_Click(object sender, RoutedEventArgs e)
+        {
+            storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            sampleFile = await storageFolder.GetFileAsync("sample.txt");
+            scoreBlock.Text = "Score = " + await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+
+            /*using (FileStream fs = new FileStream("./score.txt", FileMode.Open, FileAccess.ReadWrite))
+            {
+                StreamWriter tw = new StreamWriter(fs);
+                tw.Write("AAAAAA");
+                tw.Flush();
+            }*/
+
+         
+
+            /*using (Stream s = GenerateStreamFromString("./score.txt"))
+            {
+                using (FileStream fs = new FileStream("./score.txt", FileMode.Open, FileAccess.ReadWrite))
+                {
+                    StreamWriter tw = new StreamWriter(fs);
+                    tw.Write("AAAAAA");
+                    tw.Flush();
+                };
+            }*/
+
+
+
+
+        }
+
+        private async void connectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (myOneDrive == null)
+            {
+                myOneDrive = OneDriveClientExtensions.GetUniversalClient(scopes);
+                await myOneDrive.AuthenticateAsync();
+            }
+
+            var drive = await myOneDrive
+                                .Drive
+                                .Request()
+                                .GetAsync();
+
+            Stream stream = await sampleFile.OpenStreamForReadAsync();
+
+          //  using (var fileStream = new FileStream("./score.txt", FileMode.Open, FileAccess.Read))
+           // {
+               
+
+                var uploadedItem = await myOneDrive
+                                             .Drive
+                                             .Root
+                                             .ItemWithPath(test + " Score.txt")
+                                             .Content
+                                             .Request()
+                                             .PutAsync<Item>(stream);
+           // }
+
+            scoreBlock.Text = "Uploaded Score!";
+        }
+
+        private void playAgainBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(TypingPage));
+        }
+    }
+}
---
+++ CoolType/TypingPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CoolType
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TypingPage : Page
+    {
+        public string[] separator = { " ", "\t", "\n", "\r", "\r\n" };
+        public string[] arr = new string[] { };
+        List<string> myCollection = new List<string>();
+        public int i = 0;
+        int score = 0;
+        StorageFile file;
+        String curWord = "";
+
+        public TypingPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void listBtn_Click(object sender, RoutedEventArgs e)
+        {
+            listBlock.Text = "";
+
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.FileTypeFilter.Add(".txt");
+            file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                listBlock.Text = "Loaded File";
+                listBtn.Visibility = Visibility.Collapsed;
+                boxBtn.Visibility = Visibility.Visible;
+                listBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                listBlock.Text = "Operation Canceled";
+            }
+
+        }
+
+        private async void boxBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (file != null)
+            {
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                using (StreamReader reader = new StreamReader(stream.AsStream()))
+                {
+                    string[] words = reader.ReadToEnd().Split(' ', '\t', '\n', '\r', '\r');
+
+                    if (listBox.Text == curWord)
+                    {
+                        test.Text = "Correct";
+                        score += 1;
+                    }
+
+                    listBox.Text = "";
+                    boxBtn.Content = "Check Word";
+
+                    if (words.Length > i)
+                    {
+                        listBlock.Text = words[i]; //+ " " + words[j];
+                        curWord = words[i];
+                        int findThis = words.Length;
+                        i++;
+                    }
+                    else
+                    {
+                        Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                        Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync("sample.txt", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+
+                        await Windows.Storage.FileIO.WriteTextAsync(sampleFile, score.ToString());
+                        this.Frame.Navigate(typeof(ScorePage));
+                    }
+
+                }
+            }
+        }
+    }
+}
